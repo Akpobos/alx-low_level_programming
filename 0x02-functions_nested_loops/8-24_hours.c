2bf7863f1d68fdd0f016caeeca2ae54f3bdaec74
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-06-20 08:51:56 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute of the day
+ *
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int h = 0;
+
+	while (h < 24)
+	{
+		int m = 0;
+
+		while (m < 60)
+		{
+			if (h < 10)
+				_putchar('0');
+			else
+				_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			if (m < 10)
+				_putchar('0');
+			else
+				_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
