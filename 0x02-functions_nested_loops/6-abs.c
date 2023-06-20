39d3b17b0057d709089e74dfda721030c4d13214
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-06-20 04:21:58 -0700

    5-6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computers absolute value of an integer
+ * @n: Integer to check
+ *
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (n * -1);
+}
