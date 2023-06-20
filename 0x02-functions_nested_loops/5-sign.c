d7dc5ef6791bf7b0f6ad6c2cea4a6328f451e0c0
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-06-20 03:50:27 -0700

    4-5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Print sign of a number
+ * @n - Number to check
+ *
+ * Return: print sign and return number
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	_putchar('0');
+	return (0);
+}
