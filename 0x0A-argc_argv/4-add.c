6e2adfd4a1c726e2311ae71ce1613bc3fead174a
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-07-07 13:31:16 -0700

    ff

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,24 +8,24 @@
  */
 int main(int argc, char **argv)
 {
-        int sum = 0;
-        int c;
+	int sum = 0;
+	int c;
 
-        for (c = 1; c < argc; c++)
-        {
-                int i = 0;
+	for (c = 1; c < argc; c++)
+	{
+		int i = 0;
 
-                while (argv[c][i])
-                {
-                        if (!isdigit(argv[c][i]))
-                        {
-                                printf("Error\n");
-                                exit(EXIT_FAILURE);
-                        }
-                        i++;
-                }
-                sum += atoi(argv[c]);
-        }
-        printf("%d\n", sum);
-        exit(EXIT_SUCCESS);
+		while (argv[c][i])
+		{
+			if (!isdigit(argv[c][i]))
+			{
+				printf("Error\n");
+				exit(EXIT_FAILURE);
+			}
+			i++;
+		}
+		sum += atoi(argv[c]);
+	}
+	printf("%d\n", sum);
+	exit(EXIT_SUCCESS);
 }
