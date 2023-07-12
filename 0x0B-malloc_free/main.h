d50a6e0d485bdcfff5a736540df59827188d2871
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-07-11 18:03:31 -0700

    free grid

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -13,6 +13,7 @@ char *str_concat(char *s1, char *s2);
 unsigned int _strlen(char *str);
 void _strcat(char *dest, char *src, unsigned int start);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 /* Prototypes - END */
 
 #endif /* MAIN_H */
