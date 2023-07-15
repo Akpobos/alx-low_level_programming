661e1896974326a63f8c7c744ad1127ca269b7fa
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-07-15 02:05:02 -0700

    gg

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,6 @@ unsigned int _strlen(char *str);
 void _strcpy(char *dest, char *src, unsigned int strt, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
