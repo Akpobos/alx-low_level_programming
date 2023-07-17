34d7209361b9956cdbc37c0ab6d9d571f594b2a3
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-07-17 00:35:35 -0700

    Macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print name of current file
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	exit(EXIT_SUCCESS);
+}
