88e2686296e063d0ce2814f4da27ea2d366275a5
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-07-20 15:23:38 -0700

    fff

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers
+ * @separator: Separator
+ * @n: Numbers count
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n > 0)
+	{
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(args, int));
+			if (separator != NULL && i != (n - 1))
+				printf("%s", separator);
+			if (i == (n - 1))
+				printf("\n");
+		}
+		va_end(args);
+	}
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings
+ * @separator: Separator
+ * @n: Numbers count
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n > 0)
+	{
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			char *str = va_arg(args, char *);
+			printf("%s", str ? str : "(nil)");
+			if (separator != NULL && i != (n - 1))
+				printf("%s", separator);
+			if (i == (n - 1))
+				printf("\n");
+		}
+		va_end(args);
+	}
+}
