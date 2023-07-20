ce7f1b69e36850b4b6554a87f416f48bf70a746f
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-07-19 17:20:57 -0700

    FP

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a string
+ * @name: The string
+ * @f: Callback function
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name)
+	{
+		f(name);
+	}
+}
