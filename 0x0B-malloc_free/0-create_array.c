ceaa4ed717bedf7c31fcbe4201e670562e9b5a21
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-09-06 09:29:40 -0700

    ff

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array - Creates array of characters
+ * @size: Size of the array
+ * @c: Pre fill character
+ * Return: Pointer to array or null
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr = malloc(size + 1);
+
+	if (!ptr || !size)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	ptr[size] = '\0';
+	return (ptr);
+}
