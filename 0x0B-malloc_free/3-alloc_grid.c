b2ee9539fb65c93042a44e1820a2a2ed27d8f1b3
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-09-06 10:53:19 -0700

    ss

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,14 @@ int **alloc_grid(int width, int height)
 
 		ptr[i] = malloc(sizeof(int) * width);
 		if (!ptr[i])
+		{
+			while (i >= 0)
+			{
+				free(ptr[i]);
+			}
+			free(ptr);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	}
