61e5d709e31aff3b74629ba62a66ed62f2193fe9
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-09-06 13:20:46 -0700

    d

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * _strlen - Get str length
+ * @str: the string
+ * @Return: the length
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int c = 0;
+
+	while (*str)
+	{
+		c++;
+		str++;
+	}
+
+	return (c);
+}
+
+/**
+ * _strcp - Copy string
+ * @dest: destination
+ * @src: src
+ * @start: index to start writing
+ * Return: Nothing
+ */
+void _strcp(char *dest, char *src, int start)
+{
+	while (*src)
+	{
+		dest[start] = *src;
+		start++;
+		src++;
+	}
+}
+
+/**
+ * argstostr - Concat all argument vectors
+ * @ac: Argument count
+ * @av: Argument vector
+ * Return: The concat str
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int len = 0;
+	char *ptr;
+
+	if (!ac || !av)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		len += _strlen(av[i]);
+	len += ac + 1;
+	ptr = malloc(sizeof(char) * len);
+	if (!ptr)
+		return (NULL);
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		_strcp(ptr, av[i], len);
+		len += _strlen(av[i]);
+		ptr[len] = '\n';
+		len++;
+	}
+	ptr[len] = '\0';
+	return (ptr);
+}
