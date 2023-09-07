4f5962b2d8f6fe84ae26784990f3ad5176409e41
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-09-07 08:29:21 -0700

    rr

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,10 +12,29 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
+	ptr = malloc(sizeof(ptr) * nmemb);
 	if (!ptr)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-		ptr[i] = 0;
+	for (i = 0; i < nmemb; i++)
+	{
+		unsigned int j;
+
+		ptr[i] = malloc(size);
+		if (!ptr[i])
+		{
+			i--;
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				ptr[i] = NULL;
+				i--;
+			}
+			free(ptr);
+			ptr = NULL;
+			break;
+		}
+		for (j = 0; j < size; j++)
+			ptr[i][j] = 0;
+	}
 	return (ptr);
 }
