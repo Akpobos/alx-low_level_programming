29b211507f849c9e8ee99c32b22723b0d1c0a8c6
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-09-13 04:24:58 -0700

    f

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * main - Perform simple operation
+ * @ac: Argument count
+ * @av: Argument Vector
+ * Return: Number
+ */
+int main(int ac, char **av)
+{
+	int (*f)(int, int);
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	f = get_op_func(av[2]);
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*av[2] == '/' || *av[2] == '%') && (atoi(av[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", f(atoi(av[1]), atoi(av[3])));
+	exit(EXIT_SUCCESS);
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -10,3 +10,47 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub - Subtract two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: The result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiply two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: The result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divide two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: The result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Mod two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: The result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
