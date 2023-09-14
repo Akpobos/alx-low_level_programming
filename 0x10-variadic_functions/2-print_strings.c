a1a9f00b625b92b031039aa610bfc552c4f64e13
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-09-14 13:51:45 -0700

    rr

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_string - Prints string
+ * @separator: Separator
+ * @n: number of strings passed
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	
+	if (n)
+	{
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			char *str = va_arg(args, char *);
+
+			printf("%s", str ? str : "(nil)");
+			if (separator && i < (n - 1))
+				printf("%s", separator);
+		}
+		printf("\n");
+		va_end(args);
+	}
+}
