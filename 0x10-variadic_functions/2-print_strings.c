7240a8d00db2aef890591ed4fd465b507c3ed8c9
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-09-14 16:13:57 -0700

    ty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			char *str = va_arg(args, char *);
 
-			printf("%s", str ? str : "(nil)");
+			if (str)
+				printf("%s", str);
+			else
+				printf("(nil)");
 			if (separator && (i < (n - 1)))
 				printf("%s", separator);
 		}
