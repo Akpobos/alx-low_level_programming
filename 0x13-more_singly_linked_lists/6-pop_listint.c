dfa6510521dd7720b49e120f44de9bb7317790c4
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-09-25 15:29:11 -0700

    r

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,7 @@ int pop_listint(listint_t **head)
 	if (!head || !(*head))
 		return (0);
 	n = (*head)->n;
-	temp = (*head)->next;;
+	temp = (*head)->next;
 	free(*head);
 	*head = NULL;
 	*head = temp;
