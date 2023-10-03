4e7d578795424b9f4aab9f32d3dfaffa95f9b9cf
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-10-02 17:15:30 -0700

    e

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		if (write(fd, text_content, sizeof(*text_content) * strlen(text_content)) == -1)
+		if (
+			write(fd, text_content,
+			sizeof(*text_content) * strlen(text_content)) == -1
+		)
 		{
 			close(fd);
 			return (-1);
