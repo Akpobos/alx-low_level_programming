e283db30e32366a4ceacea621b68bec8a9ae2f14
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-11-09 05:19:14 -0800

    t

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Add node to start of list
+ * @head: points to head pointer
+ * @n: new data
+ * Return: new adderess
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	new->next = *head;
+	*head = new;
+	return (new);
+}
