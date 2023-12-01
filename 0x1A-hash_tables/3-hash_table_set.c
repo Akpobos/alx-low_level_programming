86626d2128e1f213a7ae3fcb8aa7cc7e2da6dd52
Akpobos <alabi.akpobome.emmanuel@gmail.com>
2023-12-01 01:39:11 -0800

    tt

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -54,7 +54,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 
-	if (!ht || !key || key == '\0' || !value)
+	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
 	return (create_hash_node(ht->array[index], key, value));
